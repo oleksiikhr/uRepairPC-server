878bf1460d64daedaaa8089e5707b6914b4dea4b
Alexey Khruhsch <alexeykhr@outlook.com>
2019-05-16 18:45:53 +0300

    chore: remove JWTAuth::getPayload from logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -98,7 +98,6 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => __('app.auth.logout'),
-            'asd' => JWTAuth::getPayload(),
         ]);
     }
 }
